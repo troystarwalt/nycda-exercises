9d19c7288017a1fd286d1e81862f94b494148b36
Charles Castillo <charlescastillo1982@yahoo.com>
2015-09-25 16:35:39 -0400

    added a list of people in our class

---
+++ githubUsernames.js
@@ -0,0 +1,54 @@
+// order copied from the whiteboard, no hating
+var people = [
+  { name: "Alec Stone"
+  , repo: "https://github.com/AlecStone"
+  , slackname:"" }
+, { name: "Ehtesh Choudhury"
+  , repo: "https://github.com/shurane"
+  , slackname:""  }
+, { name: "Stephanie Mathison"
+  , repo: "https://github.com/srmathison"
+  , slackname:""  }
+, { name: "Tristan Villanueva"
+  , repo: "https://github.com/BanzaiBlazer"
+  , slackname:""  }
+, { name: "Joel Santos"
+  , repo: "https://github.com/joelsantos41"
+  , slackname:""  }
+, { name: "Ade Akintide"
+  , repo: "https://github.com/adeshyne"
+  , slackname:""  }
+, { name: "Troy Starwalt"
+  , repo: "https://github.com/shurane/"
+  , slackname:""  }
+, { name: "Taimur 'Tim' Alamgir"
+  , repo: "https://github.com/talamgir"
+  , slackname:""  }
+, { name: "Jeremy Andaya"
+  , repo: "https://github.com/jeremyandaya"
+  , slackname:""  }
+, { name: "Christine Chan"
+  , repo: "https://github.com/cadence-0915/"
+  , slackname:""  }
+, { name: "Paula Murphy"
+  , repo: "https://github.com/pmmurphy"
+  , slackname:""  }
+, { name: "Michael Gabay"
+  , repo: "https://github.com/akatriel"
+  , slackname:""  }
+, { name: "Norvinder Singh"
+  , repo: ""
+  , slackname:""  }
+, { name: "Justin Bardliving"
+  , repo: ""
+  , slackname:""  }
+]
+
+var listElement = $("#list");
+for (var i=0; i<people.length; i++){
+    var element = "<li>" + people[i].name + "⇒" + people[i].repo +"</li>";
+    listElement.append(element);
+}
+
+// listElement.append("<p>hello</p>");
+// listElement.append("<p>hello</p>");
