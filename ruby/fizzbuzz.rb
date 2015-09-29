979f177299afa9cd1e8c9aebc922c25202e8e741
Charles Castillo <charlescastillo1982@yahoo.com>
2015-09-29 18:02:13 -0400

    removed fizzbuzz

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -1,12 +1,1 @@
-
-for i in (1..100) do
-    if i % 3 == 0 && i % 5 == 0
-        puts "FIZZBUZZ"
-    elsif i % 3 == 0
-        puts "FIZZ"
-    elsif i % 5 == 0
-        puts "BUZZ"
-    else
-        puts i
-    end
-end
+# REPLACE ME
