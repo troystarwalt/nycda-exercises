c326e5cf66378ee4c2e9854c595c07bbb3c81ed9
Troy Starwalt <troy.starwalt@gmail.com>
2015-09-29 13:51:28 -0400

    Added Ruby

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -1,1 +1,15 @@
-# REPLACE ME
+for i in(1..100).each do
+
+	if (i % 5===0 && i % 3===0)
+		puts "FizzBuzz"
+
+	elsif i % 3 === 0
+		puts "Fizz"
+
+	elsif i % 5 === 0
+		puts "Buzz"	
+
+	else 
+		puts i
+	end
+end
