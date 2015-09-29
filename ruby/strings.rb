95327e0a5b8c72f8755e587ccfb15da2963debc2
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2015-09-29 17:52:00 -0400

    strings.rb requirements

--- ruby/strings.rb
+++ ruby/strings.rb
@@ -1,7 +1,9 @@
+## REQUIREMENTS
+# should have to populate 10 variables and have 10 string interpolations (#{}) for this assignment.
+# The madlibs paragraph can be as long as you want, so long as there are at least 10 variables.
 
 # use `gets` to populate variables
 # replace variables in the madlib string and print it. The following is an example
-# use 
 
 # example madlib: http://cdn.rainbowresource.netdna-cdn.com/products/001471i1.jpg
 
