502d0223d35351c3c2d857aea8e4ae4741fb6314
Charles Castillo <charlescastillo1982@yahoo.com>
2015-09-30 17:46:10 -0400

    added zoo.rb exercises

---
+++ ruby/whiletrue.rb
@@ -0,0 +1,8 @@
+
+while true
+	puts "Can I have your number"
+	number = gets
+	if number.length > 9
+		break
+	end
+end
---
+++ ruby/zoo.rb
@@ -0,0 +1,8 @@
+# Exercise
+# [ ] Create 15 classes, all extend from Animal
+# [ ] make at least 3 classes such that they extend a class that also extends a class
+#     for example... Aristocat < Cat < Animal
+# [ ] use `super` on at least 4 methods that overwrite their parents' method
+# [ ] all instances of Animal have a .sound() method
+# [ ] create unique methods for each animal that other animals do not have
+#    - i.e. Cat.hairball, Snake.slytherin, Gryffin.courage, Batman.signal, Thundercat.thunder
