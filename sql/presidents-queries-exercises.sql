a39cbedb5c42ab89c03c22ed9df1c2cef36ef1ac
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2015-10-06 16:29:48 -0400

    added some SQL exercises

---
+++ sql/presidents-queries-exercises.sql
@@ -0,0 +1,11 @@
+-- 1. How to select presidents with cool names?
+--      - Cool names are when their name.length < 9
+--      - Okay names are when 9 <= name.length < 16
+--      - Bad names are when 16 <= name.length
+
+-- 2. How to select currently alive prime ministers?
+-- 3. How to select prime ministers who were in office before 1915?
+-- 4. How to select prime ministers who were in office before 1915 and after 1776?
+-- 5. How to select prime ministers who have the word 'in' in their name?
+-- 6. Of the presidents that are not alive, who were the oldest 10?
+-- 7. Of the presidents that are not alive, who were the youngest 10?
--- sql/presidents-queries.sql
+++ sql/presidents-queries.sql
@@ -1,5 +1,19 @@
+-- show all columns of all rows
 SELECT * FROM presidents;
 
+-- show only name column of all rows
+SELECT name FROM presidents;
+
+-- show president_number, name, birth_year columns of all rows
+SELECT president_number, name, birth_year FROM presidents;
+
+--  have the results sorted by a particular column, in ascending or descending order
+SELECT president_number, name, birth_year FROM presidents ORDER BY president_number ASC;
+SELECT president_number, name, birth_year FROM presidents ORDER BY president_number DESC;
+SELECT president_number, name, birth_year FROM presidents ORDER BY birth_year ASC;
+SELECT president_number, name, birth_year FROM presidents ORDER BY birth_year DESC;
+
+-- filter the results by a CONSTRAINT
 SELECT * FROM presidents WHERE party = 'Republican';
 SELECT * FROM presidents WHERE party = 'Democratic';
 
@@ -13,9 +27,9 @@ SELECT * FROM presidents WHERE party LIKE '%Democratic';
 SELECT * FROM presidents WHERE party NOT LIKE 'Democratic%';
 SELECT * FROM presidents WHERE party NOT LIKE '%Democratic';
 
--- presidents that are named 'George'
+-- presidents that are named a certain name
 SELECT * FROM presidents WHERE name LIKE '%George%';
-
+SELECT * FROM presidents WHERE name LIKE '%James%';
 -- presidents that have a 'j' in their name
 SELECT * FROM presidents WHERE LOWER(name) LIKE '%j%';
 
@@ -23,7 +37,9 @@ SELECT * FROM presidents WHERE LOWER(name) LIKE '%j%';
 SELECT * FROM presidents WHERE party = 'Whig' OR party = 'Democratic-Republican';
 SELECT * FROM presidents WHERE party = 'Republican' AND birth_year >= 1900;
 SELECT * FROM presidents WHERE party = 'Republican' AND birth_year >= 1900 AND death_year IS NULL;
+SELECT * FROM presidents WHERE name LIKE '%George%' OR name LIKE '%James%';
 
 -- can match on multiple values using IN
 SELECT * FROM presidents WHERE party IN ('Democratic', 'Republican');
 SELECT * FROM presidents WHERE party NOT IN ('Democratic', 'Republican');
+
