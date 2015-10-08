62e3661ba45208360e7f79de0a1715009a867a1e
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2015-10-08 14:02:08 -0400

    simple to add... you guys will figure it out

--- activerecord/db/seeds.rb
+++ activerecord/db/seeds.rb
@@ -3,9 +3,5 @@ require './demo.rb'
 connection = ActiveRecord::Base.connection
 
 presidents = File.read("db/presidents.sql")
-pets = File.read("db/presidentspets.sql")
-justices = File.read("db/sc_justices.sql")
 
 puts connection.execute(presidents);
-puts connection.execute(pets);
-puts connection.execute(justices);
